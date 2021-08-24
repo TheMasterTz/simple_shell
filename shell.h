96aac5b25952290c21cbce7e04c319212bfbc5c5
TheMasterTz <palavras2001@hotmail.com>
2021-08-24 13:41:44 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,31 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+typedef struct variables
+{
+	char **av;
+	char *buffer;
+	char **env;
+	char **argv;
+	int status;
+	char **commands;
+	size_t count;
+} vars_t;
+
+typedef struct builtins
+{
+	char *name;
+	void (*f)(vars_t *);
+} builtins_t;
+
+#endif
