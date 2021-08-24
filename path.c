20de97625cbd83a5d274e271cbc5c7ee93afcd9f
TheMasterTz <palavras2001@hotmail.com>
2021-08-24 13:55:23 -0700

    add function check_for_dir

--- path.c
+++ path.c
@@ -1,5 +1,17 @@
 #include "shell.h"
 
+int check_for_dir(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == '/')
+			return (1);
+	}
+	return (0);
+}
+
 void _path(vars_t *vars)
 {
 	unsigned int i = 0, iter;
