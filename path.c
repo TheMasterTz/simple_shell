7042a3b754123fcdd9f1b410b7f9545b9c14172a
TheMasterTz <palavras2001@hotmail.com>
2021-08-24 13:54:26 -0700

    add function _path

---
+++ path.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+void _path(vars_t *vars)
+{
+	unsigned int i = 0, iter;
+	char *path, *path_dup = NULL, *check_path = NULL;
+	char **path_token = NULL;
+	struct stat buff;
+
+	if (check_for_dir(vars->av[0]))
+		i = exe_path_dir(vars);
+	else
+	{
+		path = find_path(vars->env);
+		if (path != NULL)
+		{
+			path_dup = _strdup(path);
+			path_token = _tokenizer(path_dup, ":");
+			for (iter = 0; path_token[iter]; iter++)
+			{
+				check_path = _strcat(path_token[iter], vars->av[0]);
+				if (stat(check_path, &buff) == 0)
+				{
+					i = path_execute(check_path, vars);
+					free(check_path);
+					break;
+				}
+			}
+			free(path_dup);
+		}
+	}
+	if (i == 1)
+		get_exit(vars);
+}
