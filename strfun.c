03f0a4b5695cd0df4bc14a4b26a1c703324e6158
Nel10 <3461@holbertonschool.com>
2021-08-24 15:36:44 -0700

    betty style

--- strfun.c
+++ strfun.c
@@ -1,9 +1,15 @@
 #include "shell.h"
 
+/**
+ * _strlen - lenght string
+ * @str: string
+ * Return: len
+ */
 unsigned int _strlen(char *str)
 {
 	unsigned int len = 0;
 
-	for (len = 0; str[len]; len++);
+	for (len = 0; str[len]; len++)
+		;
 	return (len);
 }
